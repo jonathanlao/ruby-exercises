62ba69ab0c37297704291c061a12bb1149c77f36
Jonathan Lao <jonathan.lao1992@gmail.com>
2022-10-19 09:49:24 -0400

    caesar cipher project

---
+++ caesar_cipher.rb
@@ -0,0 +1,24 @@
+
+def caesar_cipher(str, shift)
+    cipher = str.chars.map do |char| 
+      ord = char.ord
+      if ord.between?(97, 122)
+        max = 122
+      elsif ord.between?(65, 90)
+        max = 90
+      else
+         next(char)
+      end
+      
+      ord += shift
+      while ord > max
+        ord -= 26
+      end
+
+      ord.chr
+    end
+
+    cipher.join
+end
+
+p caesar_cipher("What a stringz!", 100)
