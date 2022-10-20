7723b577bce1b68fb5f2e1e4a85a20c1b851b61a
Jonathan Lao <jonathan.lao1992@gmail.com>
2022-10-20 10:53:19 -0400

    try out rubocop

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'rspec', '~>3.9'
 gem 'debug'
+gem 'rubocop'
--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,24 +1,18 @@
-
 def caesar_cipher(str, shift)
-    cipher = str.chars.map do |char| 
-      ord = char.ord
-      if ord.between?(97, 122)
-        max = 122
-      elsif ord.between?(65, 90)
-        max = 90
-      else
-         next(char)
-      end
-      
-      ord += shift
-      while ord > max
-        ord -= 26
-      end
-
-      ord.chr
+  cipher = str.chars.map do |char|
+    ord = char.ord
+    if ord.between?(97, 122)
+      max = 122
+    elsif ord.between?(65, 90)
+      max = 90
+    else
+      next(char)
     end
-
-    cipher.join
+    ord += shift
+    ord -= 26 while ord > max
+    ord.chr
+  end
+  cipher.join
 end
 
 p caesar_cipher("What a stringz!", 100)
