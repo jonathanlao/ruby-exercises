b9f9360f35ee4be3617fef24f7d37d9c47bb5aaf
Jonathan Lao <jonathan.lao1992@gmail.com>
2022-11-21 11:58:03 -0500

    start tic tac toe

--- substrings.rb
+++ substrings.rb
@@ -8,5 +8,5 @@ def substrings(word, substrings)
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-p substrings("below", dictionary)
+p substrings('below', dictionary)
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
---
+++ tic_tac_toe.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+# Create a game of tic tac toe
+class TicTacToeBoard
+  def initialize
+    @board = Array.new(3) { Array.new(3) }
+    @turn = 'X'
+  end
+
+  MOVES = {
+    tl: [0, 0],
+    tc: [0, 1],
+    tr: [0, 2],
+    cl: [1, 0],
+    cc: [1, 1],
+    cr: [1, 2],
+    bl: [2, 0],
+    bc: [2, 1],
+    br: [2, 2]
+  }.freeze
+
+  def update_board(row, col)
+    if !@board[row][col].nil?
+      puts('Invalid move')
+    else
+      @board[row][col] = @turn
+      @turn = @turn == 'O' ? 'X' : 'O'
+    end
+  end
+
+  def move(pos)
+    pos = pos.downcase
+    if !%w[tl tr tc cl cr cc bl br bc].include? pos
+      puts('I do not recognize that move')
+    else
+      row, col = MOVES[pos.to_sym]
+      update_board(row, col)
+    end
+    print_board
+  end
+
+  def print_board
+    puts('hello!')
+  end
+end
+
+def play_game
+  game = TicTacToeBoard.new
+  game.move('cat')
+  game.move('cl')
+end
+
+play_game
